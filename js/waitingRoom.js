4543d14a94664f6631fc88eec1cef3896061d384
wheercool <wheercool@gmail.com>
2015-09-25 17:28:45 +0300

    Added waiting room speedo logic

--- js/waitingRoom.js
+++ js/waitingRoom.js
@@ -1,98 +1,97 @@
 (function() {
 	widgetRegister.register("Waiting Room", function(el, data, general, config) {
 
-	var tpl = uncomment(function(){/*
-<div class="row waiting-room">
+	var containerTpl = uncomment(function (){/*
+		<div class="row waiting-room">
 			<div class="col-md-12 clock text-center">								
-				<span>12:34</span>
-			</div>
-				
-			
+				<span>--:--</span>
+			</div>	
+
+			<div class="row speedos">
+			</div>				
 		</div>
-
-		<div class="row">
-			<div class="col-md-4">
-				<div class="panel panel-default ct-speedo" >
-					<div class="panel-heading ">CT</div>
-					<div class="panel-body">
-						<!-- svg  xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100%"><defs></defs>
-							<circle cx="125" cy="100" r="100" opacity="1" fill="gray"></circle>
-							<circle cx="125" cy="100" r="40" fill="white"></circle>
-
-
-							<path fill="none" d="M 85.00000000000001 30.717967697244887 C 109.75208614068028 16.427344100918347 140.24791385931977 16.42734410091836 165 30.717967697244887" stroke="#FAD60A" stroke-width="10px" stroke-linecap="butt"></path>
-
+		*/
+	});
+	
+	var ringTpl = uncomment(function(){
+		/*
+							<path stroke="#31a354" fill="none" d="M 45 99.99999999999999 C 44.999999999999986 71.41875280734692 60.24791385931975 45.00859129357144 84.99999999999999 30.717967697244916"  stroke-width="10px" stroke-linecap="butt"></path>
+							<path fill="none" stroke="#ccc"  stroke-width="10px" d="M 85.00000000000001 30.717967697244887 C 109.75208614068028 16.427344100918347 140.24791385931977 16.42734410091836 165 30.717967697244887"></path>
 							<path fill="none" d="M 165 30.717967697244916 C 189.75208614068026 45.00859129357145 205 71.4187528073469 205 99.99999999999999" stroke="red" stroke-width="10px" stroke-linecap="butt"></path>
+							<circle cx="125" cy="100" r="5" transform="rotate(30.036490127433755 125 100)" fill="#F0F0F0"></circle>
 
-							<path fill="none" d="M 45 99.99999999999999 C 44.999999999999986 71.41875280734692 60.24791385931975 45.00859129357144 84.99999999999999 30.717967697244916" stroke="#31a354" stroke-width="18px" stroke-linecap="round"></path>
-
-							
-							
-							<g class="arrow-indicator" transform="rotate(110 125 100)" fill="black">
-								
-								<circle cx="125" cy="100" r="10" ></circle>
-
-								<rect fill="red" x="42" y="100" width="80" height="2" stroke-width="0.000001" ></rect>
-
-								
-								
-							</g>
-						</svg -->
-						<img src="imgs/speedo.svg" width="100%" height="100%"/>
-						<h4 class="text-center">-5 min</h4>
-					</div>
-				</div>				
-				
-			</div>
-			<div class="col-md-4">
-				<div class="panel panel-default">
-					<div class="panel-heading ">Buddy</div>
-					<div class="panel-body">
-						<img src="imgs/speedo.svg" width="100%" height="100%"/>
-						<h4 class="text-center">-5 min</h4>
-					</div>
-				</div>	
-			</div>
-			<div class="col-md-4">
-				<div class="panel panel-default">
-					<div class="panel-heading ">MR</div>
-					<div class="panel-body">
-						<img src="imgs/speedo.svg" width="100%" height="100%"/>
-						<h4 class="text-center">-5 min</h4>
-					</div>
-				</div>	
-			</div>
-		</div>
-*/
+		*/
 	})
 
+
+	var indicatorTpl = uncomment(function(){
+		/*
+				<path fill="#585858" d="M 135 100 A 10 10 0 1 1 134.999999999995 99.99999 L 131.9999999999965 99.999993 A 7 7 0 1 0 132 100 Z"></path>
+
+				<rect fill="#585858" x="42" y="100" width="74" height="2" stroke-width="0.000001"></rect>
+
+				<rect fill="#585858" x="135" y="97" width="6" height="6" stroke-width="0.000001" ></rect>
+					
+
+		*/
+	})
+
+
 		d3.select(el)
-			.html(tpl);
-
-		scale = d3.scale.linear().domain([-60 * 4, 0,  60 * 4]) //4 hours
-									.range([0, 90, 180]);
-
-		var value = 5;
+			.html(containerTpl);
 
 
-		var svg = d3.select('.ct-speed svg')
+		scale = d3.scale.linear().domain([config.min, 0,  config.max]) 
+									.range([0, 90, 180])
+									.clamp(true)
+	
 
+		var panel = d3.select(el).select('.speedos')
+			.selectAll('.col-md-4')
+			.data(data)
+			.enter()
+			.append('div')
+			.classed('col-md-4', true)
+			.append('div')
+			.attr('class', 'panel panel-default')
 
-		d3.select('.ct-speedo .arrow-indicator')
-			.transition()
-			.duration(2000)
-			 .attrTween("transform", tween);
-			// .attr('transform', 'rotate(' + scale(value) + ' 120 100)')
+		var header = panel.append('div')
+			.attr('class', 'panel-heading')
+			.text(function(d) {return d.name})
 
-			 function tween(d, i, a) {
-			 	console.log(i);
-		      return d3.interpolateString("rotate(90, 125 100)", "rotate(" +  scale(value) + ", 125 100)");
-		    }
+		var body = panel.append('div')
+			.attr('class', 'panel-body text-center')
+			// .style('background-color', function(d) {
+			// 	return d.value > config.max? 'rgba(217, 83, 79, 0.0901961)': 'white';
+			// })
 
-		d3.select('.ct-speedo h4')
+		var svg = body.append('svg')
+					.attr('preserveAspectRatio', 'xMinYMin meet')
+					.attr('viewBox', '0 0 255 125')
+
+			svg.html(ringTpl)
+
+		var indicators = svg.append('g')
+			.classed('arrow-indicator', true)
+
+			indicators.html(indicatorTpl)
+
+		body.append('h4')		
 			.text(function(d) {
-				return '' + value + ' min';
+				if (d.value == 0) return 'in time';
+				return  (d.value > 0 ?'+': '') + d.value + ' min';
 			})
+
+
+		indicators
+		.transition()
+		.duration(2000)
+		 .attrTween("transform", tween);
+	
+		 function tween(d, i, a) {
+	      return d3.interpolateString("rotate(90, 125 100)", "rotate(" +  scale(d.value) + ", 125 100)");
+	    }
+
 
 	});
 
