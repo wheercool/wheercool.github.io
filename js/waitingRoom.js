5d4ae4c0c134c5336f0d9aa0a27995bdb4cb3a58
wheercool <wheercool@gmail.com>
2015-09-25 21:04:05 +0300

    Changed ring

--- js/waitingRoom.js
+++ js/waitingRoom.js
@@ -66,9 +66,23 @@
 					// .attr('preserveAspectRatio', 'xMinYMin meet')
 					.attr('viewBox', '0 0 255 125')
 
-			svg.append('text')
-				.attr('y', 50)
-				.text('Testing')
+			svg.append('path')
+				.attr('stroke', '#31a354')
+				.attr('d', "M 45 99.99999999999999 C 44.999999999999986 71.41875280734692 60.24791385931975 45.00859129357144 84.99999999999999 30.717967697244916")
+				.attr('stroke-width', '10px')
+				.attr('fill', 'none')
+
+			svg.append('path')
+				.attr('stroke', '#ccc')
+				.attr('d', "M 85.00000000000001 30.717967697244887 C 109.75208614068028 16.427344100918347 140.24791385931977 16.42734410091836 165 30.717967697244887")
+				.attr('stroke-width', '10px')
+				.attr('fill', 'none')
+
+			svg.append('path')
+				.attr('stroke', 'red')
+				.attr('d', "M 165 30.717967697244916 C 189.75208614068026 45.00859129357145 205 71.4187528073469 205 99.99999999999999")
+				.attr('stroke-width', '10px')
+				.attr('fill', 'none')
 
 			// svg.html(ringTpl)
 
